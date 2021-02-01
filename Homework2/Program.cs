11546261c0747479696f78281745bce629dba536
Марти Джордан <mrqwer2014@mail.ru>
2021-02-02 02:25:41 +0300

    Добавьте файлы проекта.

---
+++ Homework2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using MyClassLib.WorldOfTanks;
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tank[] t34 = new Tank[5];
+            Tank[] pantera = new Tank[5];
+            int vivod;
+            int countWin = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                t34[i] = new Tank("T34", rnd);
+                pantera[i] = new Tank("Pantera", rnd);
+                t34[i].coutTank();
+                pantera[i].coutTank();
+                vivod = t34[i] ^ pantera[i];
+                if(vivod == 1)
+                {
+                    WriteLine("Победа Т34");
+                    countWin++;
+                }
+                else if (vivod == 0)
+                {
+                    WriteLine("Победа пантеры");
+                }
+                else
+                {
+                    WriteLine("Оба танка уничтожены");
+                }
+            }
+            ReadKey();
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+namespace MyClassLib.WorldOfTanks
+{
+    class Tank
+    {
+        string name;
+        uint ammunition;
+        uint armor;
+        uint maneuverability;
+        public Tank(string name, Random rnd)
+        {
+            this.name = name;
+            ammunition = (uint)rnd.Next(0, 100);
+            armor = (uint)rnd.Next(0, 100);
+            maneuverability = (uint)rnd.Next(0, 100);
+        }
+
+        public static int operator ^(Tank t34, Tank pantera)
+        {
+            byte count = 0;
+            byte count2 = 0;
+            if (t34.armor > pantera.armor)
+            {
+                count++;
+            }
+            if (t34.ammunition > pantera.ammunition)
+            {
+                count++;
+            }
+            if (t34.maneuverability > pantera.maneuverability)
+            {
+                count++;
+            }
+            if (t34.armor == pantera.armor)
+            {
+                count2++;
+            }
+            if (t34.ammunition == pantera.ammunition)
+            {
+                count2++;
+            }
+            if (t34.maneuverability == pantera.maneuverability)
+            {
+                count2++;
+            }
+            if (count2 >= 2)
+            {
+                return -1;
+            }
+            if (count >= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void coutTank()
+        {
+            WriteLine(name + " " + ammunition + "% " + armor + "% " + maneuverability + "%");
+        }
+    }
+}
